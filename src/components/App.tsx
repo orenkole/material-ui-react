aaf4ac277932ebd9f3f13aaa7a12bf04b577c461
Oleh <orenkole@gmail.com>
2021-06-05 11:06:28 +0300

    14. Styling - Themes (Setup)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import Header from './UI/Header';
+import Header from './ui/Header';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from "./ui/Theme";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      {[...new Array(120)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
+      <ThemeProvider theme={theme}>
+        <Header />
+        Hello
+      </ThemeProvider>
     </div>
   );
 }
