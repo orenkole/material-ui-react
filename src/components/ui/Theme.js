f2110c5bc7a250b8f47b118f5455fedbb5b9ac95
Oleh <orenkole@gmail.com>
2021-06-05 11:33:49 +0300

    16. Styling - Themes (Palette)

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,5 +1,21 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
+const arcBlue = "#0B72B9";
+const arcOrange = "#FFBA50";
+const blackish = "#000";
+
 export default createMuiTheme({
   // values to overwrite in default theme
+  palette: {
+    common: {
+      blue: `${arcBlue}`, // new
+      orange: `${arcOrange}`// new
+    },
+    primary: {
+      main: `${arcBlue}` // overwritten
+    },
+    secondary: {
+      main: `${arcOrange}` // overwritten
+    }
+  }
 });
