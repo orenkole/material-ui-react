8796dacd85d93f1ed498f94f94737f37d6dc073b
Oleh <orenkole@gmail.com>
2021-06-06 15:44:06 +0300

    39. Using Media Queries

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { useScrollTrigger } from '@material-ui/core';
+import { useMediaQuery, useScrollTrigger } from '@material-ui/core';
 import { makeStyles } from "@material-ui/styles";
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -9,6 +9,8 @@ import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+// import useMediaQuery from "@material-ui/core/MaterialMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
 
 // import logo from "../../assets/logo.svg";
 
@@ -27,10 +29,22 @@ function ElevationScroll(props: any) {
 const useStyles = makeStyles((theme: any) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: "3em"
+    marginBottom: "3em",
+    [theme.breakpoints.down("md")]: {
+      marginBottom: "2em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginBottom: "1.5em",
+    }
   },
   logo: {
-    height: "7em",
+    height: "8em",
+    [theme.breakpoints.down("md")]: {
+      height: "7em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "5.5em",
+    }
   },
   tabContainer: {
     marginLeft: "auto"
@@ -70,6 +84,8 @@ const useStyles = makeStyles((theme: any) => ({
 function Header() {
 
   const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"))
   const [value, setValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
@@ -134,23 +150,24 @@ function Header() {
           setValue(1);
           setSelectedIndex(3)
         }
+        break;
       case "/revolution":
         if (value !== 2) {
           setValue(2);
         }
         break;
       case "/about":
-        if(value !== 3) {
+        if (value !== 3) {
           setValue(3);
         }
         break;
-      case "/contact": 
-        if(value !== 4) {
+      case "/contact":
+        if (value !== 4) {
           setValue(4);
         }
         break;
       case "/estimate":
-        if(value !== 5) {
+        if (value !== 5) {
           setValue(5);
         }
         break;
@@ -158,6 +175,84 @@ function Header() {
         break;
     }
   }, [value])
+
+  const tabs = (
+    <React.Fragment>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        className={classes.tabContainer}
+        indicatorColor="primary"
+      >
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to="/"
+          label="Home"
+        />
+        <Tab
+          aria-owns={anchorEl ? "simple-menu" : undefined}
+          aria-haspopup={anchorEl ? "true" : undefined}
+          className={classes.tab}
+          component={Link}
+          onMouseOver={(event: any) => handleClick(event)}
+          to="/services"
+          label="Services"
+        />
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to="/revolution"
+          label="The Revolution"
+        />
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to="/about"
+          label="About Us"
+        />
+        <Tab
+          className={classes.tab}
+          component={Link} to="/contact"
+          label="Contact Us"
+        />
+      </Tabs>
+      <Button variant="contained" color="secondary" className={classes.button}>
+        Free estimate
+            </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{ onMouseLeave: handleClose }}
+        classes={{ paper: classes.menu }}
+        elevation={0}
+      >
+        {
+          menuOptions.map((option: any, i: number) => {
+            return (
+              <MenuItem
+                key={option}
+                onClick={(event: any) => {
+                  handleMenuItemClick(event, i);
+                  setValue(1);
+                  handleClose(event);
+                }}
+                // menu item selecte and tab selected also
+                selected={i === selectedIndex && value === 1}
+                component={Link}
+                to={option.link}
+                classes={{ root: classes.menuItem }}
+              >
+                {option.name}
+              </MenuItem>
+            )
+          })
+        }
+      </Menu>
+    </React.Fragment>
+  )
 
   return (
     <React.Fragment>
@@ -172,79 +267,7 @@ function Header() {
             >
               <svg className={classes.logo} id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 139" > <style>{`.st0{fill:none}.st1{fill:#fff}.st2{font-family:Raleway; font-weight:300}.st6{fill:none;stroke:#000;stroke-width:3;stroke-miterlimit:10}`}</style> <path d="M448.07-1l-9.62 17.24-8.36 14.96L369.93 139H-1V-1z" /> <path className="st0" d="M-1 139h479.92v.01H-1z" /> <text transform="translate(261.994 65.233)" className="st1 st2" fontSize="57" > Arc </text> <text transform="translate(17.692 112.015)" className="st1 st2" fontSize="54" > Development </text> <path className="st0" d="M382.44 116.43l47.65-85.23 8.36-14.96M369.83 139l-.01.01L362 153" /> <path d="M438.76 15.76l-56.42 100.91c-12.52-10.83-20.45-26.82-20.45-44.67 0-32.58 26.42-59 59-59 6.23 0 12.24.97 17.87 2.76z" fill="#0b72b9" /> <path d="M479.89 72c0 32.58-26.42 59-59 59-14.73 0-28.21-5.4-38.55-14.33l56.42-100.91c23.85 7.57 41.13 29.89 41.13 56.24z" /> <g id="Group_186" transform="translate(30.153 11.413)"> <g id="Group_185"> <g id="Words"> <path id="Path_59" className="st1" d="M405.05 14.4l-.09 80.38-7.67-.01.06-52.25-29.4 52.21-7.94-.01 45.04-80.32z" /> </g> </g> </g> <path className="st0" d="M457-17l-8.93 16-9.62 17.24-8.36 14.96L369.93 139l-.01.01L361 155" /> </svg>
             </Button>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              className={classes.tabContainer}
-              indicatorColor="primary"
-            >
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/"
-                label="Home"
-              />
-              <Tab
-                aria-owns={anchorEl ? "simple-menu" : undefined}
-                aria-haspopup={anchorEl ? "true" : undefined}
-                className={classes.tab}
-                component={Link}
-                onMouseOver={(event: any) => handleClick(event)}
-                to="/services"
-                label="Services"
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/revolution"
-                label="The Revolution"
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to="/about"
-                label="About Us"
-              />
-              <Tab
-                className={classes.tab}
-                component={Link} to="/contact"
-                label="Contact Us"
-              />
-            </Tabs>
-            <Button variant="contained" color="secondary" className={classes.button}>
-              Free estimate
-            </Button>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{ onMouseLeave: handleClose }}
-              classes={{ paper: classes.menu }}
-              elevation={0}
-            >
-              {
-                menuOptions.map((option: any, i: number) => {
-                  return (
-                    <MenuItem
-                      key={option}
-                      onClick={(event: any) => {
-                        handleMenuItemClick(event, i);
-                        setValue(1);
-                        handleClose(event);
-                      }}
-                      // menu item selecte and tab selected also
-                      selected={i === selectedIndex && value === 1}
-                      component={Link}
-                      to={option.link}
-                      classes={{ root: classes.menuItem }}
-                    >
-                      {option.name}
-                    </MenuItem>
-                  )
-                })
-              }
-            </Menu>
+            {matches ? null : tabs}
           </Toolbar>
         </AppBar>
       </ElevationScroll>
