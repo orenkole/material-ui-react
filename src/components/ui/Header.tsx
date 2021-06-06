832da26d81fa368120a6c052e9b4905ba5113e6f
Oleh <orenkole@gmail.com>
2021-06-06 14:49:22 +0300

    35. Using Menus - Refresh Fix

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -99,25 +99,63 @@ function Header() {
   }
 
   const menuOptions = [
-    {name: "Services", link: "/services"},
-    {name: "Custom Software", link: "/customsoftware"},
-    {name: "Mobile App Development", link: "/mobileapps"},
-    {name: "Website Development", link: "/websites"},
+    { name: "Services", link: "/services" },
+    { name: "Custom Software", link: "/customsoftware" },
+    { name: "Mobile App Development", link: "/mobileapps" },
+    { name: "Website Development", link: "/websites" },
   ]
 
   useEffect(() => {
-    if (window.location.pathname === "/" && value !== 0) {
-      setValue(0)
-    } else if (window.location.pathname === "/services" && value !== 1) {
-      setValue(1)
-    } else if (window.location.pathname === "/revolution" && value !== 2) {
-      setValue(2)
-    } else if (window.location.pathname === "/about" && value !== 3) {
-      setValue(3)
-    } else if (window.location.pathname === "/contact" && value !== 4) {
-      setValue(4)
-    } else if (window.location.pathname === "/estimate" && value !== 5) {
-      setValue(5)
+    switch (window.location.pathname) {
+      case "/":
+        if (value !== 0) {
+          setValue(0)
+        }
+        break;
+      case "/services":
+        if (value !== 1) {
+          setValue(1)
+        }
+        break;
+      case "/customsoftware":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(1);
+        }
+        break;
+      case "/mobileapps":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(2);
+        }
+        break;
+      case "/websites":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(3)
+        }
+      case "/revolution":
+        if (value !== 2) {
+          setValue(2);
+        }
+        break;
+      case "/about":
+        if(value !== 3) {
+          setValue(3);
+        }
+        break;
+      case "/contact": 
+        if(value !== 4) {
+          setValue(4);
+        }
+        break;
+      case "/estimate":
+        if(value !== 5) {
+          setValue(5);
+        }
+        break;
+      default:
+        break;
     }
   }, [value])
 
@@ -127,9 +165,9 @@ function Header() {
         <AppBar position="fixed">
           <Toolbar disableGutters>
             <Button
-              component={Link} to="/" 
+              component={Link} to="/"
               className={classes.logoContainer}
-              onClick={() => {setValue(0)}}
+              onClick={() => { setValue(0) }}
               disableRipple
             >
               <svg className={classes.logo} id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 139" > <style>{`.st0{fill:none}.st1{fill:#fff}.st2{font-family:Raleway; font-weight:300}.st6{fill:none;stroke:#000;stroke-width:3;stroke-miterlimit:10}`}</style> <path d="M448.07-1l-9.62 17.24-8.36 14.96L369.93 139H-1V-1z" /> <path className="st0" d="M-1 139h479.92v.01H-1z" /> <text transform="translate(261.994 65.233)" className="st1 st2" fontSize="57" > Arc </text> <text transform="translate(17.692 112.015)" className="st1 st2" fontSize="54" > Development </text> <path className="st0" d="M382.44 116.43l47.65-85.23 8.36-14.96M369.83 139l-.01.01L362 153" /> <path d="M438.76 15.76l-56.42 100.91c-12.52-10.83-20.45-26.82-20.45-44.67 0-32.58 26.42-59 59-59 6.23 0 12.24.97 17.87 2.76z" fill="#0b72b9" /> <path d="M479.89 72c0 32.58-26.42 59-59 59-14.73 0-28.21-5.4-38.55-14.33l56.42-100.91c23.85 7.57 41.13 29.89 41.13 56.24z" /> <g id="Group_186" transform="translate(30.153 11.413)"> <g id="Group_185"> <g id="Words"> <path id="Path_59" className="st1" d="M405.05 14.4l-.09 80.38-7.67-.01.06-52.25-29.4 52.21-7.94-.01 45.04-80.32z" /> </g> </g> </g> <path className="st0" d="M457-17l-8.93 16-9.62 17.24-8.36 14.96L369.93 139l-.01.01L361 155" /> </svg>
@@ -140,55 +178,55 @@ function Header() {
               className={classes.tabContainer}
               indicatorColor="primary"
             >
-              <Tab 
-                className={classes.tab} 
-                component={Link} 
-                to="/" 
-                label="Home" 
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
               />
-              <Tab 
+              <Tab
                 aria-owns={anchorEl ? "simple-menu" : undefined}
                 aria-haspopup={anchorEl ? "true" : undefined}
-                className={classes.tab} 
-                component={Link} 
+                className={classes.tab}
+                component={Link}
                 onMouseOver={(event: any) => handleClick(event)}
-                to="/services" 
-                label="Services" 
+                to="/services"
+                label="Services"
               />
-              <Tab 
-                className={classes.tab} 
-                component={Link} 
-                to="/revolution" 
-                label="The Revolution" 
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
               />
-              <Tab 
-                className={classes.tab} 
-                component={Link} 
-                to="/about" 
-                label="About Us" 
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
               />
-              <Tab 
-                className={classes.tab} 
-                component={Link} to="/contact" 
-                label="Contact Us" 
+              <Tab
+                className={classes.tab}
+                component={Link} to="/contact"
+                label="Contact Us"
               />
             </Tabs>
             <Button variant="contained" color="secondary" className={classes.button}>
               Free estimate
             </Button>
-            <Menu 
+            <Menu
               id="simple-menu"
               anchorEl={anchorEl}
               open={open}
               onClose={handleClose}
-              MenuListProps={{onMouseLeave: handleClose}}
-              classes={{paper: classes.menu}}
+              MenuListProps={{ onMouseLeave: handleClose }}
+              classes={{ paper: classes.menu }}
               elevation={0}
             >
               {
                 menuOptions.map((option: any, i: number) => {
                   return (
-                    <MenuItem 
+                    <MenuItem
                       key={option}
                       onClick={(event: any) => {
                         handleMenuItemClick(event, i);
@@ -199,7 +237,7 @@ function Header() {
                       selected={i === selectedIndex && value === 1}
                       component={Link}
                       to={option.link}
-                      classes={{root: classes.menuItem}}
+                      classes={{ root: classes.menuItem }}
                     >
                       {option.name}
                     </MenuItem>
